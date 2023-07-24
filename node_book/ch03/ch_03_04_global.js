3d738440e03de9a24f5ba47760ead06f474536bc
Jake1152 <rancho974@gmail.com>
2023-07-24 19:17:30 +0900

    [ADD] ch03

---
+++ node_book/ch03/ch_03_04_global.js
@@ -0,0 +1,10 @@
+const odd = 'odd';
+const even = 'even';
+
+module.exports = { odd, even };
+// console.log(module)
+// console.log(module.exports)
+
+console.log(global)
+console.log("\n\n\n========================\n\n\n")
+console.log(globalThis)
