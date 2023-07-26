7661cc9c53381a8bb77815299a4778f3ac029df4
Jake1152 <rancho974@gmail.com>
2023-07-26 15:48:15 +0900

    [ADD] child process, file system

--- node_book/ch03/ch_03_07_path.js
+++ node_book/ch03/ch_03_07_path.js
@@ -12,15 +12,15 @@ console.log('path.basename - extname:', path.basename(string, path.extname(strin
 console.log('------------------------------');
 console.log('path.parse()', path.parse(string));
 console.log('path.format():', path.format({
-  dir: 'C:\\Users\Bluffing',
+  dir: 'C:\\users\zerocho',
   name: 'path',
   ext: '.js',
 }));
-console.log('path.normalize():', path.normalize('C://Users\\Bluffing\\path.js'));
+console.log('path.normalize():', path.normalize('C://users\\zerocho\\path.js'));
 console.log('------------------------------');
-console.log('path.isAbsolute(C:\):', path.isAbsolute('C:\\'));
+console.log('path.isAbsolute(C:\):', path.isAbsolute('C:\'));
 console.log('path.isAbsolute(./home):', path.isAbsolute('./home'));
 console.log('------------------------------');
-console.log('path.relative():', path.relative('C:\\Users\\Bluffing\\path.js', 'C:\\'));
-console.log('path.join():', path.join(__dirname, '..', '..', '/Users', '.', '/Bluffing'));
-console.log('path.resolve():', path.resolve(__dirname, '..', 'Users', '.', '/Bluffing'));
+console.log('path.relative():', path.relative('C:\users\zerocho\path.js', 'C:\'));
+console.log('path.join():', path.join(__dirname, '..', '..', '/users', '.', '/zerocho'));
+console.log('path.resolve():', path.resolve(__dirname, '..', 'users', '.', '/zerocho'));
