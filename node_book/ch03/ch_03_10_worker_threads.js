fd8dcf760de6686dea31df9dfb1f36ca3df348e3
Jake1152 <rancho974@gmail.com>
2023-07-26 14:49:50 +0900

    [ADD] more than 2 thread

--- node_book/ch03/ch_03_10_worker_threads.js
+++ node_book/ch03/ch_03_10_worker_threads.js
@@ -1,18 +1,25 @@
-// worker thread
-
-const { 
-    Worker, isMainThread, parentPort 
-} = require('worker_threads');
-
-if (isMainThread) {
-    const worker = new Worker(__filename);
-    worker.on('message', message => console.log('from worker', message));
-    worker.on('exit', () => console.log('from exit'));
-    worker.postMessage('ping');
-} else { // worker thread
-    parentPort.on('message', (value) => {
-        console.log('from parent', value);
-        parentPort.postMessage('pong');
-        parentPort.close();
-    });
-}
+const {
+    Worker, isMainThread, parentPort, workerData,
+  } = require('worker_threads');
+  
+  if (isMainThread) { // 부모일 때
+    const threads = new Set();
+    threads.add(new Worker(__filename, {
+      workerData: { start: 1 },
+    }));
+    threads.add(new Worker(__filename, {
+      workerData: { start: 2 },
+    }));
+    for (let worker of threads) {
+      worker.on('message', message => console.log('from worker', message));
+      worker.on('exit', () => {
+        threads.delete(worker);
+        if (threads.size === 0) {
+          console.log('job done');
+        }
+      });
+    }
+  } else { // 워커일 때
+    const data = workerData;
+    parentPort.postMessage(data.start + 100);
+  }
