e57b2f38e0546d9e71d50add970bba9f2da79839
Jake1152 <rancho974@gmail.com>
2023-08-03 14:04:56 +0900

    [ADD] http server and using fs version

---
+++ node_book/ch04/ch_04_02_http_server_using_fs.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const server = http.createServer(async (req, res) => {
+	try {
+		
+		res.writeHead(200, { 'Content-Type' : 'text/html; charset=utf-8'});
+		const data = await fs.readFile('server2.html');
+		res.end(data);
+	} catch (error) {
+		console.log(error);
+	}
+	// res.write('<h1>Hello Node!</h1>');
+	// res.write('<p>Hello server </p>');
+	// res.end('<p>Hello Jake </p>')
+})
+	.listen(8080); // 80번 포트는 보통 이미 쓰이고 있기에 error발생
+server.on('listening', () => {
+	console.log('Now server waiting on 8080 port.');
+});
+
+const server1 = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html; charset=uft-8'});
+	res.write('<h1>Hello Node!</h1>');
+	res.write('<p>Hello server </p>');
+	res.end('<p>Hello Jake </p>')
+})
+	.listen(8081);
+
+server.on('error', (error) => {
+	console.error(error);
+});
+
+server1.on('error', (error) => {
+	console.error(error);
+});
