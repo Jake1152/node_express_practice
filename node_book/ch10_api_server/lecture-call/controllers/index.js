5ccfd21e7641af9308a132d86a23b457e37f61c3
Jake1152 <rancho974@gmail.com>
2023-12-31 22:32:35 +0900

    [WIP] 다른 서비스 호출하기
    - api 서버로부터 받은 client key를 이용하여 서비스 호출하기

---
+++ node_book/ch10_api_server/lecture-call/controllers/index.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+exports.test = async (req, res, next) => {
+  try {
+    console.log("It is root route");
+    if (!req.session.jwt) {
+      //   res.send("Token is vailded");
+      // console.log(`#process.env.CLIENT_SECRET: ${process.env.CLIENT_SECRET}`);
+      const tokenResult = await axios.post("http://localhost:4200/v1/token", {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      /**
+       * 매번 토큰을 발급 받는 것은 낭비이므로 session에 저장한다.
+       */
+      if (tokenResult?.data?.code === 200) {
+        req.session.jwt = tokenResult.data.token;
+        //   res.send("Token is vailded");
+      } else {
+        // token 발급 실패시 처리
+        return res.status(tokenResult.data?.code).json(tokenResult.data);
+      }
+    }
+    // jwt는 headers에 authorization을 넣는다.
+    const result = await axios.get("http://localhost:4200/v1/test", {
+      headers: { authorization: req.session.jwt },
+    });
+    return res.json(result.data);
+  } catch (error) {
+    // console.error(error);
+    if (error.response?.status === 419) {
+      res.status(419).json(error.response.data);
+    }
+    return next(error);
+  }
+};
